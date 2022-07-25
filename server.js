a091e038743d66e4b2c428a1b4a3f548103b2848
adithyabathey <85573928+adithyabathey@users.noreply.github.com>
2022-07-25 16:16:23 +0530

    angular connection fixed

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express=require('express');
 var app=express();
+var cors = require('cors')
 
 app.use(express.urlencoded());
-app.use(express.json())
+app.use(express.json());
+app.use(cors('*'));
+
 
 app.listen(3000,()=>{console.log("express on localhost:3000")});
-
+app.get('/',(req,res)=>{
+    res.send('Connected');
+})
 // //all employees
 // app.get('/employees',(req,res)=>{
 //     mysqlConnection.query('SELECT * FROM Employee',(err,rows,field)=>{
